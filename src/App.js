2929c7f624073ae6157ab9c3361fdaf33f8afc2a
ankitkr88588 <141405860+ankitkr88588@users.noreply.github.com>
2024-01-08 15:33:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DeviceDropdown extends React.Component {
+  render() {
+    return (
+      <div>
+        <select>
+          <option value="infinix">Infinix</option>
+          <option value="tecno">Tecno</option>
+          <option value="itel">Itel</option>
+        </select>
+
+        {/* Insert the AdSense script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5956825596826996"
+          crossorigin="anonymous"
+        ></script>
+      </div>
+    );
+  }
 }
 
-export default App;
-    
+export default DeviceDropdown;
