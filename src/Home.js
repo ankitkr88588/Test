2ebdad4abc437359cc9a3d715bebff75d8a6ec17
ankitkr88588 <141405860+ankitkr88588@users.noreply.github.com>
2024-01-08 17:23:59 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="content">
+      <h2>Home Page</h2>
+      <p>Welcome to the Infinix page. Choose an option from the navigation above!</p>
+    </div>
+  );
+}
+
+export default Home;
