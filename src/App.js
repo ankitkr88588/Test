1920da7e611bb5d5cde81b1559a82ef8e72cd06d
ankitkr88588 <141405860+ankitkr88588@users.noreply.github.com>
2024-01-08 17:23:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import './DeviceDropdown.css'; // Import CSS for styling (create this file)
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import './App.css';
 
-class DeviceDropdown extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Welcome to Device Selector</h1>
-        <div className="dropdown">
-          <select className="dropdown-select">
-            <option value="select">Select a Device</option>
-            <option value="infinix">Infinix</option>
-            <option value="tecno">Tecno</option>
-            <option value="itel">Itel</option>
-          </select>
-        </div>
+// Components
+import Home from './components/Home';
+import RootFiles from './components/RootFiles';
+import StockRoms from './components/StockRoms';
 
-        {/* Insert the AdSense script */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5956825596826996"
-          crossorigin="anonymous"
-        ></script>
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Your Infinix Page!</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/root-files">Infinix Root Files</Link>
+            <Link to="/stock-roms">Infinix Stock ROMs</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/root-files" component={RootFiles} />
+          <Route exact path="/stock-roms" component={StockRoms} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default DeviceDropdown;
+export default App;
